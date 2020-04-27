94400bcfa985acc1c83cc17feaf701a84a6e3c4d
Javokhir169 <42350057+Javokhir169@users.noreply.github.com>
2020-04-27 09:24:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/apssystem/splashScreen.kt
@@ -0,0 +1,17 @@
+package com.example.apssystem
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splashScreen : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },  SPLASH_TIME_OUT)
+    }
+}
